d7789e32ea58a1e8cc43f0171e133b422407e38b
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-03-11 23:45:43 +0200

    commit

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const sectionRef = document.querySelector('.task-3');
-
-sectionRef.addEventListener('click', function (e) {
-    let num = Math.pow(Number(e.target.textContent),2)
-
-    e.target.textContent = num
-});
